39b302870a01ab2e7e8207c581ade3ea3d4ec7c0
NotEvenAThing <grazviuks@gmail.com>
2023-11-09 21:24:00 +0200

    main menu

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<?php
-echo "Hello kek";
-echo "Zdarova";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MB "Pamaitink"</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>Sveiki atvyke į <?php echo "MB Pamaitink"; ?></h1>
+        <button id="login">Prisijungti</button>
+    </header>
+
+    <section>
+        <h3>Kokia nors reklama goes here</h3>
+    </section>
+    <section>
+        <h3>Norite tapti lojalumo klientu? spauskite cia!</h3>
+        <button>Form</button>
+    </section>
+    <footer>
+        &copy; <?php echo date("Y"); ?> My Website. All rights reserved.
+    </footer>
+
+</body>
+</html>
